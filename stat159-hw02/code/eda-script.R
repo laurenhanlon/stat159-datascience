e53531896084ef0a24fcac8d9506e129d83a4812
Lauren Hanlon <laurenmhanlon@gmail.com>
2016-10-06 15:40:41 -0700

    fixed colors

--- stat159-hw02/code/eda-script.R
+++ stat159-hw02/code/eda-script.R
@@ -1,36 +1,30 @@
-setwd("~/Desktop/Fall2016/stat159-datascience/stat159-hw02/code")
-getwd()
-
 # Getting the data
-setwd("../")
-setwd(paste(getwd(),"/data",sep=""))
-getwd()
 
-advertising <- read.table('Advertising.csv', header=TRUE, sep=',')
+advertising <- read.table('../data/Advertising.csv', header=TRUE, sep=',')
 
-# Summary statistics
-s <- summary(advertising)
-capture.output(s, file = "eda-output.txt")
+# Summary statistics for TV and Sales
+tv_sales = c('TV', 'Sales')
+subset = advertising[tv_sales]
+
+s <- summary(subset)
+capture.output(s, file = "../data/eda-output.txt")
 
 # Histograms
-setwd("../")
-setwd(paste(getwd(),"/images",sep=""))
-getwd()
 
 ## TV
-pdf('histogram-tv.pdf')
-hist(advertising$TV, xlab='TV', breaks=20, main='TV Histogram', col='lightgreen', ylim=c(0, 20))
+pdf('../images/histogram-tv.pdf')
+hist(advertising$TV, xlab='TV (in thousands)', breaks=20, main='TV Histogram', col='blue', ylim=c(0, 20))
 dev.off()
 
-png('histogram-tv.png')
-hist(advertising$TV, xlab='TV', breaks=20, main='TV Histogram', col='lightgreen', ylim=c(0, 20))
+png('../images/histogram-tv.png')
+hist(advertising$TV, xlab='TV (in thousands)', breaks=20, main='TV Histogram', col='blue', ylim=c(0, 20))
 dev.off()
 
 ## Sales
-pdf('histogram-sales.pdf')
-hist(advertising$Sales, xlab='Sales', breaks=15, main='Sales Histogram', col='limegreen', xlim=c(0,30), ylim=c(0, 40))
+pdf('../images/histogram-sales.pdf')
+hist(advertising$Sales, xlab='Sales (in thousands)', breaks=15, main='Sales Histogram', col='lightblue', xlim=c(0,30), ylim=c(0, 40))
 dev.off()
 
-png('histogram-sales.png')
-hist(advertising$Sales, xlab='Sales', breaks=15, main='Sales Histogram', col='limegreen', xlim=c(0,30), ylim=c(0, 40))
+png('../images/histogram-sales.png')
+hist(advertising$Sales, xlab='Sales (in thousands)', breaks=15, main='Sales Histogram', col='lightblue', xlim=c(0,30), ylim=c(0, 40))
 dev.off()
