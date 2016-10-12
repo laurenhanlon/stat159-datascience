2ed373a4c720e7012d7c65dd21e687ea24740d46
Lauren Hanlon <laurenmhanlon@gmail.com>
2016-10-12 11:18:09 -0700

    session info of libraries used

---
+++ stat159-hw03/code/scripts/session-info-script.R
@@ -0,0 +1,11 @@
+
+## Library calls to all the packages used in project
+library(ggplot2)
+library(gclus)
+library(stargazer)
+library(testthat)
+
+## Session Info
+sink("../../session-info.txt")
+sessionInfo()
+sink()
